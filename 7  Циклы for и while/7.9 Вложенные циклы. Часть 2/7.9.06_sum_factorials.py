add5a1f1781900497ef5dde8238c40f20fca798b
Andrey Lutsenko <anlucenko@gmail.com>
2022-07-25 13:56:40 +0300

    10

---
+++ 7  Циклы for и while/7.9 Вложенные циклы. Часть 2/7.9.06_sum_factorials.py
@@ -0,0 +1,16 @@
+# Вариант 1: без вложенного цикла
+from math import *
+n = int(input())
+sum_factorials1 = 0
+for i in range(1, n + 1):
+    sum_factorials1 += factorial(i)
+
+# Вариант 2: с вложенным циклом
+sum_factorials2 = 0
+f = 1
+for i in range(1, n + 1):
+    for j in range(i, i + 1):
+        f *= j
+    sum_factorials2 += f
+if sum_factorials1 == sum_factorials2:
+    print(sum_factorials1)
