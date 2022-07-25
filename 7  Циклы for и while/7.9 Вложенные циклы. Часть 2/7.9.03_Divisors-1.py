fd00706d38700a68d134dd20d3a1895744cdc51e
Andrey Lutsenko <anlucenko@gmail.com>
2022-07-25 09:49:07 +0300

    7

---
+++ 7  Циклы for и while/7.9 Вложенные циклы. Часть 2/7.9.03_Divisors-1.py
@@ -0,0 +1,14 @@
+a, b = int(input()), int(input())
+number, sum_divisors = 0, 0
+for i in range(a, b + 1):
+    s = 0    
+    for j in range(1, i + 1):
+        if i%j == 0:
+            n = i
+            s += j
+#            print('n =', n, 'делитель =', j)
+#    print('n =', n, 's =', s)
+    if s >= sum_divisors and n > number:
+        sum_divisors = s
+        number = n
+print(number, sum_divisors
