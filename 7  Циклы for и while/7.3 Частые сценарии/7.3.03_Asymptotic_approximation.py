5e0307cc573dbca71dc395249b8e3d64b9726687
Andrey Lutsenko <anlucenko@gmail.com>
2022-08-01 14:04:11 +0300

    1

--- 7  Циклы for и while/7.3 Частые сценарии/7.3.03_Asymptotic_approximation.py
+++ 7  Циклы for и while/7.3 Частые сценарии/7.3.03_Asymptotic_approximation.py
@@ -1,4 +1,5 @@
 from math import *
+
 n = int(input())
 s = 0
 for i in range(1, n + 1):
--- 7  Циклы for и while/7.9 Вложенные циклы. Часть 2/7.9.02_Numerical_triangle_4.py
+++ 7  Циклы for и while/7.9 Вложенные циклы. Часть 2/7.9.02_Numerical_triangle_4.py
@@ -1,6 +1,6 @@
 n = int(input())
-for i in range(1, 2*n, 2):
+for i in range(1, 2 * n, 2):
     for j in range(1, i + 1):
-        digit = (i+1)//2 - abs(j - (i+1)//2)%n
+        digit = (i + 1) // 2 - abs(j - (i + 1) // 2) % n
         print(digit, end='')
     print()
